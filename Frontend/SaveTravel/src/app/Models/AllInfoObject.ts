e90a6a76832abb140e87302abbc55d8d9bd60dfd
your-username <thomas_jakob@hotmail.com>
2020-09-26 13:55:37 +0200

    model

---
+++ Frontend/SaveTravel/src/app/Models/AllInfoObject.ts
@@ -0,0 +1,12 @@
+export class AllInfoObject {
+
+    countryName:string;
+	countryCode:string;
+	lastUpdateTW:string;
+	travelWarningScore:number;
+	coronaScore:number;
+	coronaByPopulation:string;
+	coronaActiveCases:number;
+	}
+	
+	
